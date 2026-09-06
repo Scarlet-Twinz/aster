a34f5850477ea492a8c404a7890e5d8d0b7750ad
Anthony Emmanuella Mmasinachi <anthonyemmanuella297@gmail.com>
2026-09-05 17:41:14 -0700

    feat: represent explicit variable types in AST

--- src/ast.rs
+++ src/ast.rs
@@ -1,3 +1,5 @@
+use crate::type_system::Type;
+
 #[derive(Debug, Clone, PartialEq)]
 pub struct Program {
     pub statements: Vec<Stmt>,
@@ -5,21 +7,13 @@ pub struct Program {
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum Stmt {
-    Let { name: String, initializer: Expr },
+    Let { name: String, type_annotation: Option<Type>, initializer: Expr },
     Expression(Expr),
     Print(Expr),
     Return(Option<Expr>),
     Block(Vec<Stmt>),
-    If {
-        condition: Expr,
-        then_branch: Box<Stmt>,
-        else_branch: Option<Box<Stmt>>,
-    },
-    Function {
-        name: String,
-        params: Vec<String>,
-        body: Vec<Stmt>,
-    },
+    If { condition: Expr, then_branch: Box<Stmt>, else_branch: Option<Box<Stmt>> },
+    Function { name: String, params: Vec<String>, body: Vec<Stmt> },
 }
 
 #[derive(Debug, Clone, PartialEq)]
@@ -28,44 +22,18 @@ pub enum Expr {
     String(String),
     Boolean(bool),
     Variable(String),
-    Unary {
-        operator: UnaryOp,
-        operand: Box<Expr>,
-    },
-    Binary {
-        left: Box<Expr>,
-        operator: BinaryOp,
-        right: Box<Expr>,
-    },
-    Assign {
-        name: String,
-        value: Box<Expr>,
-    },
-    Call {
-        callee: Box<Expr>,
-        arguments: Vec<Expr>,
-    },
+    Unary { operator: UnaryOp, operand: Box<Expr> },
+    Binary { left: Box<Expr>, operator: BinaryOp, right: Box<Expr> },
+    Assign { name: String, value: Box<Expr> },
+    Call { callee: Box<Expr>, arguments: Vec<Expr> },
 }
 
 #[derive(Debug, Clone, Copy, PartialEq)]
-pub enum UnaryOp {
-    Negate,
-    Not,
-}
+pub enum UnaryOp { Negate, Not }
 
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub enum BinaryOp {
-    Add,
-    Subtract,
-    Multiply,
-    Divide,
-    Modulo,
-    Equal,
-    NotEqual,
-    Less,
-    LessEqual,
-    Greater,
-    GreaterEqual,
-    And,
-    Or,
+    Add, Subtract, Multiply, Divide, Modulo,
+    Equal, NotEqual, Less, LessEqual, Greater, GreaterEqual,
+    And, Or,
 }
