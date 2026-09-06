a6950e1d851f1a5dcafbfbf042a9d4332c8b49c2
Anthony Emmanuella Mmasinachi <anthonyemmanuella297@gmail.com>
2026-09-05 17:25:34 -0700

    Wire ASTER semantic analysis into compiler API

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,20 @@
 pub mod ast;
 pub mod lexer;
 pub mod parser;
+pub mod semantic;
 pub mod token;
+pub mod type_system;
 
 use ast::Program;
 use lexer::{LexError, Lexer};
 use parser::{ParseError, Parser};
+use semantic::{analyze, SemanticError};
 
 #[derive(Debug)]
 pub enum CompileError {
     Lex(Vec<LexError>),
     Parse(Vec<ParseError>),
+    Semantic(Vec<SemanticError>),
 }
 
 pub fn parse_source(source: &str) -> Result<Program, CompileError> {
@@ -21,4 +25,10 @@ pub fn parse_source(source: &str) -> Result<Program, CompileError> {
     Parser::new(tokens)
         .parse()
         .map_err(CompileError::Parse)
+}
+
+pub fn analyze_source(source: &str) -> Result<Program, CompileError> {
+    let program = parse_source(source)?;
+    analyze(&program).map_err(CompileError::Semantic)?;
+    Ok(program)
 }
